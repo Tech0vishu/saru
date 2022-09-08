ecb47ecce30ea864a8dd340475b12f8e7c0e4d40
vishvastp <36479442+vishvastp@users.noreply.github.com>
2022-09-08 14:09:37 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "username";
-$password = "password";
+$password =  "password";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
